226f1345a98ca908d28937b1b3ab92a28bd3d910
Bashar Shehadeh <bashar.shehadeh@gnteq.com>
2024-11-17 00:48:27 +0300

    update delete and create in basket

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndPoints.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndPoints.cs
@@ -1,6 +1,23 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketEndPoints
+    public record DeleteBasketRequest(string UserName);
+    public record DeleteBasketResponse(bool IsSuccess);
+    public class DeleteBasketEndPoints : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/DeleteBasket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteBasketCommand(userName));
+                var response = result.Adapt<DeleteBasketResponse>();
+                return Results.Ok(response);
+            })
+                   .WithDescription("Delete Basket")
+                   .WithName("Delete Basket")
+                   .WithSummary("Delete product in basket")
+                   .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+                   .ProducesProblem(StatusCodes.Status404NotFound);
+        }
     }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndPoints.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndPoints.cs
@@ -1,6 +1,26 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketEndPoints
+    public record StoreBasketRequest(ShoppingCart cart);
+    public record StoreBasketResponse(string UserName);
+    public class StoreBasketEndPoints : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBasketCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<StoreBasketResponse>();
+
+                return Results.Created($"/basket/{response.UserName}", response);
+            })
+                .WithDescription("Create Product")
+                .WithName("CreateProduct")
+                .WithSummary("Create Product")
+                .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest);
+
+        }
     }
 }
